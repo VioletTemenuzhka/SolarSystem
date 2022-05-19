9b1e4939fd5111b88fa806cd5f9610076d94db0b
VioletTemenuzhka <violet.chan.fl@gmail.com>
2022-05-19 00:38:40 -0400

    The feature Sun has been added successfully

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -9,5 +9,11 @@ public class Main {
         FactorX.moon="4";
         FactorX.planet="2";
         FactorX.stars="10000";
+
+        // adding features to our sun
+        FeatureSun Xfact = new FeatureSun();
+        Xfact.color ="green";
+        Xfact.radius = "10000000000000km";
+        Xfact.heat ="33443556666kj";
     }
 }
